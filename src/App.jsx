b1bad5ed20d4de87f47bb27c0f9ef18204915b56
Harish Kamuni <108679987+HarishKamuni@users.noreply.github.com>
2024-05-19 10:37:17 +0530

    create weather app using react js,tailwind css,vite and react-unicons

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import UilReact from '@iconscout/react-unicons/icons/uil-react';
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-2xl font-bold text-red-500">Hello World</h1>
+        <UilReact size={55} />
+      </div>
+    </>
+  );
+}
+
+export default App;
